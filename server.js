c3f85b39889fe7d250114ee263715ccbc4707dc8
ascotson <ascotson@gmail.com>
2016-01-13 19:16:24 -0700

    Steps 1 & 2 completed.

--- server.js
+++ server.js
@@ -1,1 +1,45 @@
+var express = require('express');
 
+var app = express();
+
+var bodyParser = require('body-parser');
+
+var cors = require('cors');
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+var port = 3000;
+
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
+});
+
+var messages = ['Hello dolly!', 'Hello Frank!', 'Are you still here?'];
+
+app.get('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+});
+
+app.post('/', function(req, res) {
+  messages.push(req.body.message);
+
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+});
