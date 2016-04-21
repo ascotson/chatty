2963093aaf70fa8d13caf3684ab6a6c2235751b8
ascotson <ascotson@gmail.com>
2016-04-21 17:30:25 -0600

    Project refactored, completed, & both front end and backend were successfully tested. Angular date filter was also added for UI improvement.

--- server.js
+++ server.js
@@ -1,24 +1,46 @@
 var express = require('express');
-
 var app = express();
 
 var bodyParser = require('body-parser');
 
-var cors = require('cors');
-
 app.use(bodyParser.json());
 
-app.use(cors());
-
 var port = 3000;
-
-app.listen(port, function() {
-  console.log('Listening on port ' + port);
+app.listen(port, function(){
+  console.log('Listening on Port ' + port);
 });
 
-var messages = ['Hello dolly!', 'Hello Frank!', 'Are you still here?'];
+var messages = [];  //where should I store this array so the data persists between requests?
 
-app.get('/', function(req, res) {
+app.options('/', function(req, res){
+  res.status(200).set({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+      'X-XSS-Protection': '1; mode=block',
+      'X-Frame-Options': 'SAMEORIGIN',
+      'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send();
+});
+
+app.get('/', function(req, res){
+  res.status(200).set({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+      'X-XSS-Protection': '1; mode=block',
+      'X-Frame-Options': 'SAMEORIGIN',
+      'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send(messages);
+});
+
+app.post('/', function(req, res){
+  messages.push({
+    time: new Date(),
+    message: req.body.message
+  });
   res.status(200).set({
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
@@ -27,19 +49,6 @@ app.get('/', function(req, res) {
     'X-XSS-Protection': '1; mode=block',
     'X-Frame-Options': 'SAMEORIGIN',
     'Content-Security-Policy': "default-src 'self' devmountain.github.io"
-  }).send(JSON.stringify(messages));
-});
-
-app.post('/', function(req, res) {
-  messages.push(req.body.message);
-
-  res.status(200).set({
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-    'X-XSS-Protection': '1; mode=block',
-    'X-Frame-Options': 'SAMEORIGIN',
-    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
-  }).send(JSON.stringify(messages));
+  }).send(messages);
+    console.log('The POST was delivered!');
 });
